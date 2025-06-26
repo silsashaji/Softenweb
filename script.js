80976dcb51855c7ff07aff329c313e0e2f5ac401
EMildaBabu <emildababu2002@gmail.com>
2025-06-27 01:37:18 +0530

    intial commit

--- script.js
+++ script.js
@@ -47,6 +47,28 @@ $(document).ready(function () {
         backSpeed: 60,
         loop: true
     });
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
  
 
 });
